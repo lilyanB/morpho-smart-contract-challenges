0c5b91c21259d66c6b1aa442b508d63977bb8a1c
lilyanb <lilyan.bastien@gmail.com>
2026-01-16 11:21:04 +0100

    perf: finish TokenHeuristic

--- src/gas-optis/g03-token-heuristic/Optimized.sol
+++ src/gas-optis/g03-token-heuristic/Optimized.sol
@@ -37,17 +37,37 @@ contract TokenHeuristic is ITokenHeuristic {
     }
 
     function score(TokenIndicators tokenA, TokenIndicators tokenB) external view returns (uint256) {
-        return (_score(tokenA) + _score(tokenB)) * 1000
-            / (_score(tokenA) > _score(tokenB) ? _score(tokenA) - _score(tokenB) : _score(tokenB) - _score(tokenA));
+        GlobalCoefficientsProvider globalCoefficientsProvider = _globalCoefficientsProvider;
+        (uint256 scoreA, uint256 scoreB) = _scores(tokenA, tokenB, globalCoefficientsProvider);
+        return (scoreA + scoreB) * 1000 / (scoreA > scoreB ? scoreA - scoreB : scoreB - scoreA);
     }
 
-    function _score(TokenIndicators token) internal view returns (uint256 s) {
-        s += (_pcPrice + _globalCoefficientsProvider.gcPrice()) * token.price();
-        s += (_pcVolume + _globalCoefficientsProvider.gcVolume()) * token.volume();
-        s += (_pcVolatility + _globalCoefficientsProvider.gcVolatility()) * token.volatility();
-        s += (_pcMarketCap + _globalCoefficientsProvider.gcMarketCap()) * token.marketCap();
-        s += (_pcHolders + _globalCoefficientsProvider.gcHolders()) * token.holders();
-        s += (_pcTotalTransfers + _globalCoefficientsProvider.gcTotalTransfers()) * token.totalTransfers();
-        s += (_pcAge + _globalCoefficientsProvider.gcAge()) * token.age();
+    function _scores(
+        TokenIndicators tokenA,
+        TokenIndicators tokenB,
+        GlobalCoefficientsProvider globalCoefficientsProvider
+    ) internal view returns (uint256 sA, uint256 sB) {
+        uint256 price = (_pcPrice + globalCoefficientsProvider.gcPrice());
+        uint256 volume = (_pcVolume + globalCoefficientsProvider.gcVolume());
+        uint256 volatility = (_pcVolatility + globalCoefficientsProvider.gcVolatility());
+        uint256 marketCap = (_pcMarketCap + globalCoefficientsProvider.gcMarketCap());
+        uint256 holders = (_pcHolders + globalCoefficientsProvider.gcHolders());
+        uint256 totalTransfers = (_pcTotalTransfers + globalCoefficientsProvider.gcTotalTransfers());
+        uint256 age = (_pcAge + globalCoefficientsProvider.gcAge());
+
+        sA += price * tokenA.price();
+        sA += volume * tokenA.volume();
+        sA += volatility * tokenA.volatility();
+        sA += marketCap * tokenA.marketCap();
+        sA += holders * tokenA.holders();
+        sA += totalTransfers * tokenA.totalTransfers();
+        sA += age * tokenA.age();
+        sB += price * tokenB.price();
+        sB += volume * tokenB.volume();
+        sB += volatility * tokenB.volatility();
+        sB += marketCap * tokenB.marketCap();
+        sB += holders * tokenB.holders();
+        sB += totalTransfers * tokenB.totalTransfers();
+        sB += age * tokenB.age();
     }
 }

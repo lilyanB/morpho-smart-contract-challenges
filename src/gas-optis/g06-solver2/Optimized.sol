c6c4866d75357c593d32686eb809c0582ad514ee
lilyanb <lilyan.bastien@gmail.com>
2026-01-21 21:33:10 +0100

    perf: Solver2 version 1

--- src/gas-optis/g06-solver2/Optimized.sol
+++ src/gas-optis/g06-solver2/Optimized.sol
@@ -5,29 +5,38 @@ import {GuessTheNumber2, ISolver2} from "./Common.sol";
 
 contract Solver2 is ISolver2 {
     function solve(GuessTheNumber2 game) external returns (uint256) {
-        uint256 min = 0;
-        uint256 max = type(uint256).max;
-        while (max - min >= 1) {
-            uint256 middle = min + (max - min) / 2;
-            try this.cheat(game, middle) {
-                revert();
-            } catch Error(string memory err) {
-                GuessTheNumber2.Result result = abi.decode(bytes(err), (GuessTheNumber2.Result));
-                if (result == GuessTheNumber2.Result.GREATER) {
-                    min = middle + 1;
-                } else if (result == GuessTheNumber2.Result.LESS) {
-                    max = middle - 1;
-                } else {
-                    min = middle;
-                    max = middle;
+        uint256 value;
+        assembly {
+            // from "https://docs.soliditylang.org/en/latest/assembly.html#example"
+            // retrieve the size of the code, this needs assembly
+            let size := extcodesize(game)
+            // allocate output byte array - this could also be done without assembly
+            // by using code = new bytes(size)
+            let code := mload(0x40)
+            // new "memory end" including padding
+            mstore(0x40, add(code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+            // store length in memory
+            mstore(code, size)
+            // actually retrieve the code, this needs assembly
+            extcodecopy(game, add(code, 0x20), 0, sub(size, 160)) // we skip the last 160 bytes
+
+            // we can see all the bytecode of the contract and analyze it to find 2 push32. These push32
+            // correspond to the 2 immutables used during the "guess" function.
+
+            let codePtr := add(code, 0x20)
+            let codeLen := mload(code)
+
+            // scan through the bytecode by checking each 32-byte sequence
+            for { let i := 0 } lt(i, sub(codeLen, 32)) { i := add(i, 1) } {
+                let byteVal := byte(0, mload(add(codePtr, i)))
+                // if we find a PUSH32 opcode (0x7f)
+                if eq(byteVal, 0x7f) {
+                    // retrieve the next 32 bytes as the immutable value
+                    value := mload(add(codePtr, add(i, 1)))
+                    break
                 }
             }
         }
-        return min;
-    }
-
-    function cheat(GuessTheNumber2 game, uint256 guess) external {
-        GuessTheNumber2.Result result = game.guess(guess);
-        revert(string(abi.encode(result)));
+        return value;
     }
 }
